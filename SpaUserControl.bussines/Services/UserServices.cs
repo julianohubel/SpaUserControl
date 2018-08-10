3c7fae05215afee13baaa57e3525366ec2cdae42
Juliano Hubel <juliano_hubel@hotmail.com>
2018-08-09 22:59:55 -0300

    Services

---
+++ SpaUserControl.bussines/Services/UserServices.cs
@@ -0,0 +1,89 @@
+﻿using SpanUserControl.common.Resources;
+using SpanUserControl.common.Validation;
+using SpaUserControl.domain.Contracts.Services;
+using SpaUserControlDataContex.domain.Contracts.Reopositories;
+using SpaUserControlDataContex.domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaUserControl.bussines.Services
+{
+    public class UserServices : IUserService
+    {
+        private IUserRepository _repository;
+
+        public UserServices(IUserRepository repository)
+        {
+            this._repository = repository;
+        }
+
+        public User Authenticate(string email, string password)
+        {
+            var user = GetByEmail(email);
+
+            if (user.Password != PasswordAssertionConcern.Encrypt(password))
+                throw new Exception(Erros.InvalidPassword);
+
+            return user;
+        }
+
+        public void ChangeInformation(string email, string name)
+        {
+            var user = GetByEmail(email);
+
+            user.ChangeName(name);
+            user.Validate();
+
+            _repository.Update(user);
+
+        }
+
+        public void ChangePassword(string email, string password, string newPassword, string confirmPassword)
+        {
+            var user = Authenticate(email, password);
+            user.SetPassWord(newPassword, confirmPassword);
+
+            user.Validate();
+
+            _repository.Update(user);
+        }
+
+
+        public User GetByEmail(string email)
+        {
+            return _repository.Get(email);
+        }
+
+        public void Register(string name, string email, string password, string confirmPassword)
+        {
+            var hasUser = GetByEmail(email);
+            if (hasUser != null)
+                throw new Exception(Erros.DuplicateEmail);
+
+            var user = new User(name, email);
+            user.SetPassWord(password, confirmPassword);
+            user.Validate();
+
+            _repository.Create(user);
+        }
+
+        public string resetPassword(string email)
+        {
+            var user = GetByEmail(email);
+
+            string password = user.ResetPassword();
+
+            user.Validate();
+
+            _repository.Update(user);
+            return password;
+        }
+        public void Dispose()
+        {
+            _repository.Dispose();
+        }
+    }
+}
---
+++ SpaUserControl.domain/Contracts/Services/IUserService.cs
@@ -0,0 +1,21 @@
+﻿using SpaUserControlDataContex.domain.Model;
+using System;
+
+namespace SpaUserControl.domain.Contracts.Services
+{
+    public interface IUserService : IDisposable
+    {
+        User Authenticate(string email, string password);
+
+        User GetByEmail(string email);
+
+        void Register(string name, string email, string password, string confirmPassword);
+
+        void ChangeInformation(string email, string name);
+
+        void ChangePassword(string email, string password, string newPassword, string confirmPassword);
+
+        string resetPassword(string email);
+
+    }
+}
